ffd476c00b505bfe561ab2a64d143fe63124ad31
GuoXiCheng <1377994267@qq.com>
2022-07-25 22:27:14 +0800

    add function getNodeText

--- app/src/main/java/com/android/oneclick/service/MyAccessibilityService.kt
+++ app/src/main/java/com/android/oneclick/service/MyAccessibilityService.kt
@@ -3,12 +3,33 @@ package com.android.oneclick.service
 import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 
 class MyAccessibilityService: AccessibilityService() {
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         Log.i("AccessibilityService", "onAccessibilityEvent")
+        if (rootInActiveWindow != null) {
+            getNodeText(rootInActiveWindow)
+        }
     }
     override fun onInterrupt() {
         Log.i("AccessibilityService", "onInterrupt")
+    }
+
+    private fun getNodeText(accessibilityNodeInfo: AccessibilityNodeInfo) {
+        val childCounts = accessibilityNodeInfo.childCount
+        if (childCounts == 0) {
+            if (accessibilityNodeInfo.text != null) {
+                Log.i("getNodeText", accessibilityNodeInfo.text.toString())
+            }
+            return
+        }
+        for (i in 0 until childCounts) {
+            val node = accessibilityNodeInfo.getChild(i)
+            if (node != null) {
+                getNodeText(node)
+            }
+        }
+        return
     }
 }
