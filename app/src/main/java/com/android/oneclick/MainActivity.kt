b62ad6bcbc8226aaf9abc8bed0f9fa287cc6f756
GuoXiCheng <1377994267@qq.com>
2022-07-31 11:00:15 +0800

    add initiate mode

--- app/src/main/java/com/android/oneclick/MainActivity.kt
+++ app/src/main/java/com/android/oneclick/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.android.oneclick
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import com.android.oneclick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private var accessibilityEnabled = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (accessibilityEnabled != 1) {
+                val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+                this.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        accessibilityEnabled = Settings.Secure.getInt(
+            this.applicationContext.contentResolver,Settings.Secure.ACCESSIBILITY_ENABLED)
+        if (accessibilityEnabled == 1) binding.button.text = R.string.initiate_mode.toString()
     }
 }
