fb3903e2192cae36f5c9b493139d8c0cb908dce9
GuoXiCheng <1377994267@qq.com>
2023-11-28 22:38:10 +0800

    add StartButton

--- app/src/main/java/com/android/skip/NewMainActivity.kt
+++ app/src/main/java/com/android/skip/NewMainActivity.kt
@@ -1,14 +1,39 @@
 package com.android.skip
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.skip.compose.StartButton
 
 class NewMainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        setContent{}
+        setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(vertical = 64.dp, horizontal = 32.dp)
+            ) {
+                StartButton()
+            }
+
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+
 }
