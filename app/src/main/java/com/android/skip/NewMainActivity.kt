dd9831a9c54f88fb135359c9e246521cbdd30e4f
GuoXiCheng <1377994267@qq.com>
2023-11-30 22:40:51 +0800

    add viewmodel

--- app/src/main/java/com/android/skip/NewMainActivity.kt
+++ app/src/main/java/com/android/skip/NewMainActivity.kt
@@ -2,6 +2,7 @@ package com.android.skip
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -13,8 +14,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.skip.compose.StartButton
+import com.android.skip.viewmodel.StartButtonViewModel
 
 class NewMainActivity : AppCompatActivity() {
+    private val startButtonViewModel: StartButtonViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,10 +28,15 @@ class NewMainActivity : AppCompatActivity() {
                     .background(Color.White)
                     .padding(vertical = 64.dp, horizontal = 32.dp)
             ) {
-                StartButton()
+                StartButton(startButtonViewModel)
             }
 
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startButtonViewModel.changeButtonState(MyUtils.isAccessibilitySettingsOn(this))
     }
 }
 
--- app/src/main/java/com/android/skip/compose/StartButton.kt
+++ app/src/main/java/com/android/skip/compose/StartButton.kt
@@ -10,36 +10,31 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.android.skip.R
+import com.android.skip.viewmodel.StartButtonViewModel
+
 
 @Composable
-fun StartButton() {
+fun StartButton(viewModel: StartButtonViewModel) {
     //    val context = LocalContext.current
-    var backgroundColor by remember { mutableStateOf(Color(0xFF808080)) } // 初始背景色为灰色
-    var buttonText by remember { mutableStateOf("已停止\n点此启动") }
-    var iconResource by remember { mutableStateOf(R.drawable.block) }
+    val state by viewModel.buttonState.collectAsState()
 
     ExtendedFloatingActionButton(
         modifier = Modifier
             .fillMaxWidth()
             .height(80.dp),
-        text = { StartButtonContent(iconResource = iconResource, buttonText = buttonText) },
+        text = { StartButtonContent(iconResource = state.iconResource, buttonText = state.buttonText) },
         onClick = {
-            backgroundColor = Color(0xFF1E4377)
-            buttonText = "运行中"
-            iconResource = R.drawable.check_circle
+
         },
         icon = {},
-        containerColor = backgroundColor // 使用背景色状态
+        containerColor = state.backgroundColor // 使用背景色状态
     )
 }
 
