1302266e297c6fd5ceb7a274aa62b1e4d29aa481
GuoXiCheng <1377994267@qq.com>
2023-12-08 22:31:16 +0800

    update svg and ScaffoldPage

--- app/src/main/java/com/android/skip/AboutActivity.kt
+++ app/src/main/java/com/android/skip/AboutActivity.kt
@@ -5,53 +5,35 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.android.skip.compose.ConfirmDialog
+import com.android.skip.compose.ScaffoldPage
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AboutActivityInterface()
+            AboutActivityInterface{
+                finish()
+            }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutActivityInterface() {
+fun AboutActivityInterface(onClick: ()->Unit) {
     val showDialog = remember { mutableStateOf(false) }
     val context = LocalContext.current
 
-    Scaffold(topBar = {
-        SmallTopAppBar(title = {
-            Text("关于")
-        })
-    }, content = { contentPadding ->
-        Column(
-            Modifier
-                .padding(contentPadding)
-                .padding(15.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-
+    ScaffoldPage("关于", onClick = onClick, content = {
             TextButton(onClick = { showDialog.value = true }) {
                 Text(text = "开源地址: https://github.com/GuoXiCheng/SKIP")
             }
-        }
     })
 
     if (showDialog.value) {
---
+++ app/src/main/java/com/android/skip/compose/ScaffoldPage.kt
@@ -0,0 +1,45 @@
+package com.android.skip.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.android.skip.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldPage(barTitle: String, onClick: () -> Unit, content: @Composable (PaddingValues) -> Unit) {
+    Scaffold(topBar = {
+        SmallTopAppBar(
+            modifier = Modifier.padding(15.dp, 15.dp, 0.dp, 0.dp),
+            title = { Text(barTitle) }, navigationIcon = {
+                IconButton(onClick = onClick) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.arrow_back),
+                        contentDescription = "ArrowBackButton",
+                        tint = Color.Black
+                    )
+                }
+            })
+    }, content = { contentPadding ->
+        Column(
+            Modifier
+                .padding(contentPadding)
+                .padding(15.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            content(contentPadding)
+        }
+    })
+}
