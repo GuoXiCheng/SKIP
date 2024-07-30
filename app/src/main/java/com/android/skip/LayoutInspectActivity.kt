d1985520262ba40fe22798d055410b3cf73857ec
GuoXiCheng <1377994267@qq.com>
2024-07-30 21:56:42 +0800

    update LayoutInspectActivity

--- app/src/main/java/com/android/skip/LayoutInspectActivity.kt
+++ app/src/main/java/com/android/skip/LayoutInspectActivity.kt
@@ -1,8 +1,14 @@
 package com.android.skip
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
+import com.android.skip.compose.FlatButton
+import com.android.skip.compose.ResourceIcon
+import com.android.skip.compose.RowContent
 import com.android.skip.compose.ScaffoldPage
+import com.android.skip.utils.DataStoreUtils
 
 class LayoutInspectActivity : BaseActivity() {
 
@@ -16,7 +22,48 @@ class LayoutInspectActivity : BaseActivity() {
 
 @Composable
 fun LayoutInspectInterface(onBackClick: () -> Unit) {
-    ScaffoldPage(stringResource(id = R.string.layout_inspect), onBackClick = onBackClick, content = {
 
+    val checkFloatingWindow = remember {
+        mutableStateOf(
+            DataStoreUtils.getSyncData(
+                SKIP_FLOATING_WINDOW, false
+            )
+        )
+    }
+
+    val checkScreenCapture = remember {
+        mutableStateOf(
+            DataStoreUtils.getSyncData(
+                SKIP_SCREEN_CAPTURE, false
+            )
+        )
+    }
+
+    ScaffoldPage(stringResource(id = R.string.layout_inspect), onBackClick = onBackClick, content = {
+        FlatButton(content = {
+            RowContent(
+                stringResource(id = R.string.inspect_screen_capture_title),
+                stringResource(id = R.string.inspect_screen_capture_subtitle),
+                { ResourceIcon(iconResource = R.drawable.screenshot_keyboard) },
+                checkScreenCapture.value,
+                {
+                    checkScreenCapture.value = it
+                    DataStoreUtils.putSyncData(SKIP_SCREEN_CAPTURE, it)
+                }
+            )
+        })
+
+        FlatButton(content = {
+            RowContent(
+                stringResource(id = R.string.inspect_floating_window_title),
+                stringResource(id = R.string.inspect_floating_window_subtitle),
+                { ResourceIcon(iconResource = R.drawable.float_landscape_2) },
+                checkFloatingWindow.value,
+                {
+                    checkFloatingWindow.value = it
+                    DataStoreUtils.putSyncData(SKIP_FLOATING_WINDOW, it)
+                }
+            )
+        })
     })
 }
