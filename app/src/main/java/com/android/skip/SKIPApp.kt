37dd65973440d27a8c1565b335b0cc55631c5569
Xicheng Guo <xicheng_guo@wistron.com>
2024-08-07 16:07:39 +0800

    log with LogUtil

--- app/src/main/java/com/android/skip/SKIPApp.kt
+++ app/src/main/java/com/android/skip/SKIPApp.kt
@@ -1,11 +1,24 @@
 package com.android.skip
 
 import android.app.Application
+import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.Utils
 
 class SKIPApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
+
+        LogUtils.getConfig()
+            .setLogSwitch(true)       // 是否输出日志开关
+            .setConsoleSwitch(true)   // 是否在控制台输出日志开关
+            .setGlobalTag("SKIP_APP")   // 全局标签
+            .setLog2FileSwitch(true)      // 是否写入日志文件开关
+            .setDir("")               // 日志文件目录，默认为空，放在 /storage/emulated/0/Android/data/com.android.skip/cache/logs/
+            .setFilePrefix("log")     // 日志文件前缀
+            .setBorderSwitch(true)    // 日志边框开关
+            .setConsoleFilter(LogUtils.V) // 控制台过滤器
+            .setFileFilter(LogUtils.V)    // 文件过滤器
+            .setStackDeep(1)          // 栈深度
     }
 }
