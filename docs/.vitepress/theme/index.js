758fa6a7504add0c8b8003845234655d421cc5fc
Xicheng Guo <xicheng_guo@wistron.com>
2024-08-20 10:25:07 +0800

    add tailwind css

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,6 +2,8 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import 'element-plus/dist/index.css'
 import ElementPlus from "element-plus";
+import '../index.css'
+
 export default {
     ...DefaultTheme,
     enhanceApp({ app }) {
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './docs/.vitepress/**/*.{js,ts,vue}',
+    './docs/**/*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
