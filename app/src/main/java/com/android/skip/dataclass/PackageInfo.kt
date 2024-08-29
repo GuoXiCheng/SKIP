b146c24ca38aa2f902e9ff2fcddfc2ea473d104d
Xicheng Guo <xicheng_guo@wistron.com>
2024-08-29 14:21:08 +0800

    update

--- app/src/main/java/com/android/skip/dataclass/PackageInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.android.skip.dataclass
-
-import android.graphics.Rect
-
-data class PackageInfo(
-    val package_name: String, val skip_text: String?,
-    val skip_id: String?, var skip_rect_list: MutableList<Rect>,
-    val skip_bounds: List<String>?, val max_click_count: Int?
-)
