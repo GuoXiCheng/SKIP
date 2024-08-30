b7707e6ac8ec1b33538a5f3c6ea5c670b17bf15c
Xicheng Guo <xicheng_guo@wistron.com>
2024-08-30 14:00:56 +0800

    update MainActivity

--- app/src/main/java/com/android/skip/ui/MainActivity.kt
+++ app/src/main/java/com/android/skip/ui/MainActivity.kt
@@ -3,13 +3,48 @@ package com.android.skip.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.android.skip.R
+import com.android.skip.ui.theme.AppTheme
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            AppTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(vertical = 64.dp, horizontal = 32.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    AppTitle()
+                }
+            }
         }
     }
+}
+
+@Composable
+fun AppTitle() {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colorScheme.onBackground
+    )
 }
