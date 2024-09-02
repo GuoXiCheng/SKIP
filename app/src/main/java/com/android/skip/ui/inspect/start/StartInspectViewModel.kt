54835aca90174c06286ebbd09fdc55598952640e
Xicheng Guo <xicheng_guo@wistron.com>
2024-09-02 14:32:44 +0800

    update InspectActivity

--- app/src/main/java/com/android/skip/ui/inspect/start/StartInspectViewModel.kt
+++ app/src/main/java/com/android/skip/ui/inspect/start/StartInspectViewModel.kt
@@ -3,12 +3,14 @@ package com.android.skip.ui.inspect.start
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.skip.service.InspectService
+import com.blankj.utilcode.util.ServiceUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class StartInspectViewModel @Inject constructor(): ViewModel() {
-    private val _inspectState = MutableLiveData(false)
+    private val _inspectState = MutableLiveData(ServiceUtils.isServiceRunning(InspectService::class.java))
     val inspectState: LiveData<Boolean> = _inspectState
 
     fun changeInspectState(isEnable: Boolean) {
