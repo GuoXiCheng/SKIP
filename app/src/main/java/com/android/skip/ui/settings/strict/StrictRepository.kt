454a7082df0dcc49c773c9febac5eb70123d4aef
Xicheng Guo <xicheng_guo@wistron.com>
2024-09-19 19:00:59 +0800

    update

--- app/src/main/java/com/android/skip/ui/settings/strict/StrictRepository.kt
+++ app/src/main/java/com/android/skip/ui/settings/strict/StrictRepository.kt
@@ -2,15 +2,22 @@ package com.android.skip.ui.settings.strict
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.android.skip.R
+import com.android.skip.util.DataStoreUtils
+import com.blankj.utilcode.util.StringUtils.getString
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class StrictRepository @Inject constructor(){
-    private val _enable = MutableLiveData(false)
+class StrictRepository @Inject constructor() {
+    private val _enable = MutableLiveData(
+        DataStoreUtils.getSyncData(getString(R.string.store_strict_mode), false)
+    )
+
     val enable: LiveData<Boolean> = _enable
 
     fun changeEnable(enable: Boolean) {
         _enable.postValue(enable)
+        DataStoreUtils.putSyncData(getString(R.string.store_strict_mode), enable)
     }
 }
--- app/src/main/java/com/android/skip/ui/settings/tip/TipRepository.kt
+++ app/src/main/java/com/android/skip/ui/settings/tip/TipRepository.kt
@@ -2,15 +2,22 @@ package com.android.skip.ui.settings.tip
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.android.skip.R
+import com.android.skip.util.DataStoreUtils
+import com.blankj.utilcode.util.StringUtils.getString
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TipRepository @Inject constructor(){
-    private val _enable = MutableLiveData(false)
+class TipRepository @Inject constructor() {
+    private val _enable = MutableLiveData(
+        DataStoreUtils.getSyncData(getString(R.string.store_skip_tip), false)
+    )
+
     val enable: LiveData<Boolean> = _enable
 
     fun changeEnable(enable: Boolean) {
         _enable.postValue(enable)
+        DataStoreUtils.putSyncData(getString(R.string.store_skip_tip), enable)
     }
 }
