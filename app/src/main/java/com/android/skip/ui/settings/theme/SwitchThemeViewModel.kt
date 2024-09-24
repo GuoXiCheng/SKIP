facf83dd1f7feb97c6bdcfb74aec58c32117d0bc
Xicheng Guo <xicheng_guo@wistron.com>
2024-09-24 10:52:45 +0800

    update SwitchTheme

--- app/src/main/java/com/android/skip/ui/settings/theme/SwitchThemeViewModel.kt
+++ app/src/main/java/com/android/skip/ui/settings/theme/SwitchThemeViewModel.kt
@@ -2,8 +2,15 @@ package com.android.skip.ui.settings.theme
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.skip.R
 import com.android.skip.dataclass.SwitchThemeCarrier
+import com.android.skip.util.DataStoreUtils
+import com.blankj.utilcode.util.StringUtils.getString
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,6 +19,13 @@ class SwitchThemeViewModel @Inject constructor(
 ) : ViewModel() {
     val currentTheme: LiveData<SwitchThemeCarrier> = switchThemeRepository.currentTheme
 
-    fun changeCurrentTheme(currentTheme: SwitchThemeCarrier) =
+    fun changeCurrentTheme(currentTheme: String) {
         switchThemeRepository.changeCurrentTheme(currentTheme)
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                DataStoreUtils.putData(getString(R.string.store_current_theme), currentTheme)
+            }
+        }
+    }
 }
